733cd7a9ea604ff0f0e152b99b0ba4020eea7a9d
imen <rtibimen07@gmail.com>
2024-07-14 10:47:32 +0100

    first commit

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   base:"/salati/",
   plugins: [react()],
+  build: {
+    outDir: 'dist', // This should be the output directory
+  },
 })
